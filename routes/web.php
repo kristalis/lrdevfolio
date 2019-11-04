78e0ab6e9dde0a840e37658c126d58721d99d574
kristalis <kmabora@hotmail.com>
2019-11-04 14:57:05 +0000

    cpanel database

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,3 @@ Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource ('/heroes', 'HerocpanelController'); 
-=======
-Route::resource ('/heroes', 'HerocpanelController'); 
->>>>>>> cpanel
