4a0b7db0abb28b13a0cdf6a557df4d04d01db9d6
kristalis <kmabora@hotmail.com>
2019-11-04 18:39:35 +0000

    cpanel

--- resources/views/cpanel/herocpanel.blade.php
+++ resources/views/cpanel/herocpanel.blade.php
@@ -12,6 +12,20 @@
                         @csrf
 
                         <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Brand') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{$herolists->brand}}" required>
+
+                                @if ($errors->has('name'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Hero Title') }}</label>
 
                             <div class="col-md-6">
